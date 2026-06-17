03464db1ae622f92e2e1a16221430d7c4969843a
20335A0505 <jkatta@msa.miraclesoft.com>
2026-06-17 17:20:56 +0530

    june 17 trg update

---
+++ src/main/java/com/bean/TrgFileValidatorBean.java
@@ -0,0 +1,25 @@
+package com.bean;
+
+import java.io.File;
+import java.nio.file.Path;
+
+import org.apache.camel.Exchange;
+import org.apache.camel.language.simple.SimpleLanguage;
+public class TrgFileValidatorBean {
+	
+	public void validator(Exchange e) {
+		String filename = e.getIn().getHeader("CamelFileNameOnly").toString();
+		String folder = e.getIn().getHeader("CamelFileParent").toString();
+//		String name = FilenameUtils.removeExtension(fileName);
+		String baseName =e.getContext()
+		        .resolveLanguage("simple")
+		        .createExpression("${file:name.noext}")
+		        .evaluate(e, String.class);
+		Path trgFile = folder+baseName+".trg";
+		System.err.println(e.getFromEndpoint() != null ? e.getFromEndpoint().getEndpointUri() : null);
+		if(Files.exists(trgFile)) {
+			
+		}
+	}
+
+}
